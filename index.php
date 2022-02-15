4b9741a4edca762e87e6f38747bdef5970cc42d2
Kauê Jacyntho <kauekaj@gmail.com>
2022-02-15 19:14:16 -0300

    Integrando dados ao HTML

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Listagem de Jogos</title>
+</head>
+<body>
+    <?php
+        require_once "includes/banco.php";
+    ?>
+     
+    <div id="corpo">
+        <h1>Escolha seu jogo</h1>
+        <table class="listagem">
+            <?php
+                $busca = $banco->query("select * from jogos order by nome");
+                if(!$busca) {
+                    echo "<tr><td>Infelizmente a busca deu errado</tr>";
+                } else {
+                    if($busca->num_rows == 0) {
+                        echo "<tr><td>Nenhum registro encontrado";
+                    } else {
+                        while($reg = $busca->fetch_object()) {
+                            echo "<tr><td>$reg->capa<td>$reg->nome";
+                            echo "<td>Adm";
+                        }
+                    }
+                }
+            ?>
+        </table>
+    </div>
+    <?php $banco->close();?>
+</body>
+</html>
