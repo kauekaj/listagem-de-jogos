3f11ea377bd701c05602c37b79e9c52c45d9e833
Kauê Jacyntho <kauekaj@gmail.com>
2022-02-16 18:37:36 -0300

    ordenando dados

--- index.php
+++ index.php
@@ -11,19 +11,41 @@
     <?php
         require_once "includes/banco.php";
         require_once "includes/funcoes.php";
+        $order = $_GET['o'] ?? "n";
     ?>
      
     <div id="corpo">
         <?php include_once "topo.php" ?>
         <h1>Escolha seu jogo</h1>
         <form method="get" id="busca" action="index.php">
-            Ordenar: Nome | Produtora | Nota Alta | Nota Baixa |
+            Ordenar:
+            <a href="index.php?o=n">Nome</a> |
+            <a href="index.php?o=p">Produtora</a> |
+            <a href="index.php?o=n1">Nota Alta</a> |
+            <a href="index.php?o=n2">Nota Baixa</a> |
             Buscar: <input type="text" name="c" size="10" maxlength="40">
             <input type="submit" value="Ok">
         </form>
         <table class="listagem">
             <?php
-                $q = "select j.cod, j.nome, g.genero, p.produtora, j.capa from jogos j join generos g on j.genero = g.cod join produtoras p on j.produtora = p.cod";
+                $q = "select j.cod, j.nome, g.genero, p.produtora, j.capa from jogos j join generos g on j.genero = g.cod join produtoras p on j.produtora = p.cod ";
+
+               switch ($ordem) {
+                   case "p":
+                        $q .= "ORDER BY p.produrora";
+                        break;
+                   case "n1":
+                        $q .= "ORDER BY j.nota DESC";
+                        break;
+                   case "n2":
+                        $q .= "ORDER BY j.nota ASC";
+                        break;
+                   default:
+                        $q .= "ORDER BY j.nome";
+                        break;
+                        
+               }
+
                 $busca = $banco->query($q);
                 if(!$busca) {
                     echo "<tr><td>Infelizmente a busca deu errado</tr>";
