9b96de12bedf56d133cf71b483a292487d5e3db5
Kauê Jacyntho <kauekaj@gmail.com>
2022-02-17 11:07:35 -0300

    implementando criptografia básica para as senhas

--- includes/login.php
+++ includes/login.php
@@ -1,13 +1,21 @@
 <?php
 
+function cripto($senha) {
+    $c = '';
+    for($pos = 0; $pos < strlen($senha); $pos++) {
+        $letra = ord($senha[$pos]) + 1;
+        $c .= chr($letra);
+    }
+    return $c;
+}
+
 function gerarHash($senha) {
-    $hash = password_hash($senha, PASSWORD_DEFAULT);
+    $txt = cripto($senha);
+    $hash = password_hash($txt, PASSWORD_DEFAULT);
     return $hash;
 }
 
 function testarHash($senha, $hash) {
-    $ok = password_verify($senha, $hash);
+    $ok = password_verify(cripto($senha), $hash);
     return $ok;
 }
-
-echo gerarHash('teste');
