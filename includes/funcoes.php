189bbee51f6ba8f0afeb4b17c2310b9446bd17ab
Kauê Jacyntho <kauekaj@gmail.com>
2022-02-17 08:33:07 -0300

    criando sistema de notificações

--- includes/funcoes.php
+++ includes/funcoes.php
@@ -9,5 +9,20 @@
     }
 
     function voltar() {
-        return "<a href='index.php'><span class='material-icons black01'>arrow_circle_left</span></a>";
+        return "<a href='index.php'><span class='material-icons md-20 black01'>arrow_circle_left</span></a>";
     }
+
+    function msg_sucesso($m) {
+        $resp = "<div class='sucesso'><i class='material-icons'>check_circle</i>$m</div>";
+        return $resp;
+    } 
+
+    function msg_aviso($m) {
+        $resp = "<div class='aviso'><i class='material-icons'>info</i>$m</div>";
+        return $resp;
+    } 
+
+    function msg_erro($m) {
+        $resp = "<div class='erro'><i class='material-icons'>error</i>$m</div>";
+        return $resp;
+    } 
