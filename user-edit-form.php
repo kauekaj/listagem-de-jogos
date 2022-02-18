5472b12e9394f28370b8a06193dc07d01b5ca2d1
Kauê Jacyntho <kauekaj@gmail.com>
2022-02-18 20:36:25 -0300

    alterando e salvando dados no banco

--- user-edit-form.php
+++ user-edit-form.php
@@ -1,15 +1,21 @@
+<?php
+    $q = " SELECT usuario, nome, senha, tipo FROM usuarios WHERE usuario='" . $_SESSION['user'] . "'";
+    $busca = $banco->query($q);
+    $reg = $busca->fetch_object();
+?>
+
 <h1>Alteração de Dados</h1>
 
 <form action="user-edit.php" method="post">
     <table>
         <tr><td>Usuário
-            <td><input type="text" name="usuario" id="usuario" size="10" maxlength="10">
+            <td><input type="text" name="usuario" id="usuario" size="10" maxlength="10" value="<?php echo $reg->usuario?>" readonly>
         <tr><td>Nome    
-            <td><input type="text" name="nome" id="nome" size="30" maxlength="30">
+            <td><input type="text" name="nome" id="nome" size="30" maxlength="30" value="<?php echo $reg->nome?>">
         <tr><td>Tipo
-            <td><input type="text" name="tipo" id="tipo" size="10" maxlength="10" readonly>    
+            <td><input type="text" name="tipo" id="tipo" size="10" maxlength="10" value="<?php echo $reg->tipo?>" readonly>    
         <tr><td>Senha
-            <td><input type="password" name="senha1" id="senha1" size="10" maxlength="10">  
+            <td><input type="password" name="senha1" id="senha1" size="10" maxlength="10" >  
         <tr><td>Confirme a senha
             <td><input type="password" name="senha2" id="senha2" size="10" maxlength="10">    
         <tr><td><input type="submit" value="Salvar">
--- user-edit.php
+++ user-edit.php
@@ -23,11 +23,40 @@
                 if(!isset($_POST['usuario'])) {
                     include "user-edit-form.php";
                 } else {
-                    echo msg_sucesso("Dados foram recebidos");
-                }
+                    $usuario = $_POST['usuario'] ?? null;
+                    $nome = $_POST['nome'] ?? null;
+                    $tipo = $_POST['tipo'] ?? null;
+                    $senha1 = $_POST['senha1'] ?? null;
+                    $senha2 = $_POST['senha2'] ?? null;
+
+                    $q = "UPDATE usuarios set usuario = '$usuario', nome = '$nome'";
+
+                    if (empty($senha1) || is_null($senha1)) {
+                        echo msg_aviso("Senha antiga foi mantida.");
+                    } else {
+                        if ($senha1 === $senha2) {
+                            $senha = gerarHash($senha1);
+                            $q .= ", senha= '$senha'";
+                        } else {
+                            echo msg_erro("Senhas não conferem. A senha anterior será mantida.");
+                        }
+                    }
+
+                    $q .= " WHERE usuario = '" . $_SESSION['user'] . "'";
+                    
+                    if ($banco->query($q)) {
+                        echo msg_sucesso("Usuário alterado com sucesso!");
+                        logout();
+                        echo msg_aviso("Por segurança, efetue o <a href='user-login.php'>login</a>novamente.");
+                    } else {
+                        echo msg_erro("Não foi possível alterar os dados.");
+                    }
+
+;                }
             }
              
         ?>
+        <?php echo voltar(); ?>
     </div>
     <?php require_once "rodape.php"; ?>
 </body>
